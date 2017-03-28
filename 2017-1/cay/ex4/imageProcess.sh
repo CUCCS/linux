6d3505d9932e708c84460f8513f1c544a179bf94
AeroOwl <cucwhitney@yahoo.com>
2017-03-28 21:39:52 +0800

    实验4 编写bash脚本

---
+++ 2017-1/cay/ex4/imageProcess.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+input=""
+output=""
+quality_pct="75"
+resolution_pct="25"
+text=""
+prefix=""
+suffix=""
+isCompressQuality="0"
+isCompressResolution="0"
+isTextWatermark="0"
+isPrefix="0"
+isSuffix="0"
+isTransJPG="0"
+
+
+function Usage
+
+{
+    echo "Usage:"
+    echo "  -i  --input <filename>              Input image for processing"
+    echo "  -o  --output <filename>             Output image for saving"
+    echo "  -q, --quality <percent>             Image quality compression, default 75%, input and output are needed"
+    echo "  -r, --resolution <percent>          Image resolution compression, default 25%, input and output are needed"
+    echo "  -w, --watermark <text>              Add text watermark, input and output are needed"
+    echo "  -p, --prefix <prefix>               Add prefix"
+    echo "  -s, --suffix <suffix>               Add suffix"
+    echo "  -t, --transfer                      Transfer image format, input and output are needed"
+    echo "  -h,  --help"
+}
+
+function compressQuality
+{
+    $(convert $1 -quality $2% $3)
+    echo " Compress $1 into $3"      
+}
+function compressResolution
+{
+
+    $(convert $1 -resize $2% $3)
+    echo " Compress $1 into $3"  
+
+}
+function addTextWatermark
+{
+    $(convert $1 -draw "gravity east fill black  text 0,12 $2 " $3) 
+    echo "$3 contains the text:$2"
+}
+function transFormat
+{
+    
+    $(convert $1 $2)
+    echo "Transer $1 into $2"
+}
+function addPrefix
+{
+    for name in `ls *`
+	do
+	cp $name $1.${name}
+	done
+}
+function addSuffix
+{
+    for name in `ls *`
+        do
+        cp $name ${name}.$1
+        done
+}
+
+
+
+####################### Main ############################
+
+# Option analysis and parameters
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -i|--input)   echo "Option i, argument \`$2'" ;
+		      case "$2" in
+		          "") echo "parameter is needed" ; break ;;
+			  *)  input=$2; shift 2 ;;
+		      esac ;;
+                     
+        -o|--output)  echo "Option o, argument \`$2'" ;
+                      case "$2" in
+		  	  "") echo "parameter is needed" ; break ;;
+			  *)  output=$2; shift 2 ;;
+		      esac ;;
+
+        -q|--quality)         echo "Option q, argument \`$2'" ;
+			      quality_pct=$2 ; isCompressQuality="1" ; shift 2 ;;
+                      
+
+        -r|--resolution)      echo "Option r, argument \`$2'" ;
+			      resolution_pct=$2 ; isCompressResolution="1" ; shift 2 ;;
+                              
+
+        -w|--watermark)   echo "Option w, argument \`$2'" ;
+			      text=$2 ; isTextWatermark="1"	 ; shift 2 ;;		  
+			     
+   		  	       
+	-p|--prefix)	      echo "Option p " ;
+			      case "$2" in
+                                  "") echo "parameter is needed" ; break ;;
+			          *)  isPrefix="1" ; prefix=$2 ; shift 2 ;;	  
+			      esac ;;	
+		
+			
+	-s|--suffix)	      echo "Option s " ;
+			      case "$2" in
+				  "") echo "parameter is needed" ; break ;;
+				  *)  isSuffix="1" ; suffix=$2 ; shift 2 ;;  
+			      esac ;;
+			      
+	-t|--transfer) 	      echo "Option f" ;
+                              isTransFormat="1"
+                              shift ;;
+
+        -h|--help)	      Usage
+                       	      exit
+                       	      ;;
+
+	\?)                   Usage
+                              exit 1 ;;
+
+    esac
+   
+done
+
+
+# Execution of options
+
+if [ "$isCompressQuality" == "1" ] ; then
+	compressQuality $input $quality_pct $output
+fi
+
+if [ "$isCompressResolution" == "1" ] ; then
+        compressResolution $input $resolution_pct $output
+fi
+
+if [ "$isTransJPG" == "1" ] ; then
+        transFormat $input $output
+fi
+
+if [ "$isTextWatermark" == "1" ] ; then
+        addTextWatermark $input $text $output
+fi
+
+if [ "$isPrefix" == "1"  ] ; then
+        addPrefix $prefix
+fi
+
+if [ "$isSuffix" == "1" ] ; then
+        addSuffix $suffix
+fi
+
+
---
+++ 2017-1/cay/ex4/logAnalysis.sh
@@ -0,0 +1,93 @@
+# Top 100
+
+echo "------ # Top 100 hosts and according frequencies # -------"
+echo -e
+top100Host=$(more web_log.tsv | awk -F '\t' '{print $1}'| sort | uniq -c | sort -k1 -nr | head -n 100)
+echo "$top100Host"
+
+
+echo "---- # Top 100 hosts' IP and according frequencies # -----"
+echo -e
+top100IP=$(more web_log.tsv | awk -F '\t' '{print $1}' | egrep '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}' | sort | uniq -c | sort -k1 -nr | head -n 100)
+echo "$top100IP"
+
+
+echo "--- # Top 100 busiest URLs and according frequencies # ---"
+echo -e
+top100URL=$(more web_log.tsv |awk -F '\t' '{print $5}' | sort | uniq -c | sort -k1 -nr | head -n 100)
+echo "$top100URL"
+
+function RespStats
+{
+
+	respCode=$(more +2 web_log.tsv |awk -F'\t' '{print $6}'| sort | uniq -c | sort -nr | head -n 10 | awk '{print $2}')
+
+	respCount=$(more +2 web_log.tsv |awk -F'\t' '{print $6}'| sort | uniq -c |sort -nr | head -n 10 | awk '{print $1}')
+
+	code=($respCode)
+	count=($respCount)
+
+	sum=0
+	 for i in $respCount
+	 do
+		sum=$((${sum}+${i}))
+	done
+
+	p=0
+	for k in ${count[@]}
+	do	
+		ratio[${p}]=$(echo "scale=4; 100*${k}/$sum"|bc)
+		let p+=1
+	done
+	
+	echo -e
+	echo -e "----- # Response Code Statistics # -----"
+	echo "----------------------------------------"
+	echo -e
+	for i in $(seq 0 $(echo "${#count[@]}-1"|bc))
+	do
+		echo "Response Code: ${code[${i}]}"
+		echo "Response Count: ${count[${i}]}"
+		echo "Proportion: ${ratio[${i}]}%"
+	done
+	echo -e
+
+
+	# Top10 Url Over 4xx
+	# Top1/our/lover/4xx
+
+	temp=$(more web_log.tsv | awk -F'\t' '{if(substr($6,1,1)==4)print $5"\t"$6}' > target2.txt)
+	codes_type=$(more target2.txt | awk -F'\t' '{print $2}'| sort | uniq -c | awk '{print $2}')
+	codes_count=$(more target2.txt | awk -F'\t' '{print $2}'| sort | uniq -c | awk '{print $1}')
+
+	# 404 403
+	for t in $codes_type
+	do	
+		
+		echo -e "-------# Top 10 urls for response code $t # -------"
+		echo -e
+		echo "| Frequency |"
+		echo -e
+		url=$(more target2.txt | awk -F'\t' '{if($2=='$t')print $1}' | sort | uniq -c | sort -nr | head)	
+		echo "$url"
+		echo -e
+	done
+	
+	
+
+	# Specify a url then find out top 100 hosts ( non-interactive )
+	
+	url="/images/NASA-logosmall.gif"
+	
+	echo -e
+	echo "----- # Top 100 hosts which visited $url # ------"
+	echo -e
+	echo "| frequency |"
+	echo -e 
+	hosts=$(more web_log.tsv | awk -F'\t' '{if("'$url'"==$5)print $1}' | sort | uniq -c | sort -k1 -nr |head -n 100)
+	echo "$hosts"
+
+	
+}
+
+RespStats
